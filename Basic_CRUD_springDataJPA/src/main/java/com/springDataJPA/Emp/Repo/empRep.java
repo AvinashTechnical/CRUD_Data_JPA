cc555e8e53145c300eaea78e1f8300c657bb9af6
AvinashTechnical <technical.avinash.gupta001@gmail.com>
2025-06-18 16:50:17 +0530

    some changes on repo

--- Basic_CRUD_springDataJPA/src/main/java/com/springDataJPA/Emp/Repo/empRep.java
+++ Basic_CRUD_springDataJPA/src/main/java/com/springDataJPA/Emp/Repo/empRep.java
@@ -3,5 +3,6 @@ package com.springDataJPA.Emp.Repo;
 import com.springDataJPA.Emp.Emp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface empRep extends JpaRepository<Emp,Integer> {
 }
